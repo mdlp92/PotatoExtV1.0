94797ec49027dcec6544e34c320d37b5e0b0afa8
daddehs1 <sdaddeh2@gmail.com>
2020-03-25 10:50:07 -0700

    Fix typo in the Customer constructor

--- nodejs-express-mysql/App/models/customer.model.js
+++ nodejs-express-mysql/App/models/customer.model.js
@@ -9,8 +9,8 @@ const Customer = function(customer) {
   this.last_name = customer.last_name;
   this.date = customer.date;
   this.time = customer.time;
-  this.content_link = time.content_link;
-  this.content_title = this.content_title;
+  this.content_link = customer.content_link;
+  this.content_title = customer.content_title;
   // this.email = customer.email;
   // this.name = customer.name;
   // this.active = customer.active;
@@ -24,8 +24,14 @@ Customer.create = (newCustomer, result) => {
       return;
     }
 
-    console.log("created customer: ", { id: res.insertId, ...newCustomer });
-    result(null, { id: res.insertId, ...newCustomer });
+    console.log("created customer: ", {
+      id: res.insertId,
+      ...newCustomer
+    });
+    result(null, {
+      id: res.insertId,
+      ...newCustomer
+    });
   });
 };
 
@@ -44,7 +50,9 @@ Customer.findById = (customerId, result) => {
     }
 
     // not found Customer with the id
-    result({ kind: "not_found" }, null);
+    result({
+      kind: "not_found"
+    }, null);
   });
 };
 
@@ -62,26 +70,39 @@ Customer.getAll = result => {
 };
 
 Customer.updateById = (id, customer, Content) => {
-  sql.query(
-    "UPDATE customers SET email = ?, first_name = ?, last_name = ?, date = ?, time = ?, content_link = ?, content_title = ?, WHERE id = ?",
-    [customer.email, customer.first_name, customer.last_name, customer.date, customer.time, customer.content_link, customer.content_title, customer.customer_IDid],
-    (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-
-      if (res.affectedRows == 0) {
-        // not found Customer with the id
-        result({ kind: "not_found" }, null);
-        return;
-      }
-
-      console.log("updated customer: ", { id: id, ...customer });
-      result(null, { id: id, ...customer });
+  sql.query("UPDATE customers SET email = ?, first_name = ?, last_name = ?, date = ?, time = ?, content_link = ?, content_title = ?, WHERE id = ?", [
+    customer.email,
+    customer.first_name,
+    customer.last_name,
+    customer.date,
+    customer.time,
+    customer.content_link,
+    customer.content_title,
+    customer.customer_IDid
+  ], (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
     }
-  );
+
+    if (res.affectedRows == 0) {
+      // not found Customer with the id
+      result({
+        kind: "not_found"
+      }, null);
+      return;
+    }
+
+    console.log("updated customer: ", {
+      id: id,
+      ...customer
+    });
+    result(null, {
+      id: id,
+      ...customer
+    });
+  });
 };
 
 Customer.remove = (id, result) => {
@@ -94,7 +115,9 @@ Customer.remove = (id, result) => {
 
     if (res.affectedRows == 0) {
       // not found Customer with the id
-      result({ kind: "not_found" }, null);
+      result({
+        kind: "not_found"
+      }, null);
       return;
     }
 
